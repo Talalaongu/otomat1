88bde741a764d3f0698918b27f17795325495122
Talalaongu <50053618+Talalaongu@users.noreply.github.com>
2019-04-27 23:21:50 +0700

    Add files via upload

---
+++ OTOMAT/OTOMAT/FA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTOMAT
+{
+    class FA
+    {
+        private int _NumState;
+        private int _NumAlpabet;
+        public string[] Alphabet;
+        public State[] Q;
+        public int _FirstQ;
+        public State[] F;
+
+        public FA(int _state, int _alphabet)
+        {
+            _NumAlpabet = _alphabet;
+            _NumState = _state;
+            Alphabet = new string[_alphabet];
+            Q = new State[_state];
+            _FirstQ = 0;
+            // chưa xong
+        }
+    }
+}
---
+++ OTOMAT/OTOMAT/State.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTOMAT
+{
+    class State
+    {
+        private int _maxlength;
+        public bool[] value;
+
+        public State(int length)
+        {
+            _maxlength = length;
+            value = new bool[length];
+            for(int i = 0; i < length; i++)
+            {
+                value[i] = false;
+            }
+        }
+
+        public static State operator +(State a, State b)
+        {
+            State kq = new State(a._maxlength);
+            for(int i = 0;i < kq._maxlength; i++)
+            {
+                if(a.value[i] == true || b.value[i] == true)
+                {
+                    kq.value[i] = true;
+                }
+            }
+            return kq;
+        }
+
+        public static State operator *(State a, State b)
+        {
+            State kq = new State(a._maxlength);
+            for (int i = 0; i < kq._maxlength; i++)
+            {
+                if (a.value[i] == true && b.value[i] == true)
+                {
+                    kq.value[i] = true;
+                }
+            }
+            return kq;
+        }
+
+        public static State operator =(State a, State b)
+        {
+            State kq = new State(a._maxlength);
+            for (int i = 0; i < kq._maxlength; i++)
+            {
+                if (a.value[i] == true && b.value[i] == true)
+                {
+                    kq.value[i] = true;
+                }
+            }
+            return kq;
+        }
+    }
+}
